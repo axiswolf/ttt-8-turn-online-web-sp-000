fe2002cee5f4f23ad2834ed43c42dcbad47453cc
Karen V <karenvo@usc.edu>
2019-01-31 15:04:34 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,18 @@
+valid_move = nil
+def valid_move?(board, index)
+  # return true if move valid, false or nil if not valid
+  if (index >= 0 && index < 8 && position_taken?(board,index) == false) || (index < 8 && index > 0 && board == [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+    valid_move = true
+  else
+    valid_move = false
+  end
+end
+valid_move
+def position_taken?(board,index)
+   board[index] == " " || "" || nil # false
+   (board[index] == "X") || (board[index] == "O") # true
+end
+
 def display_board(board)
   puts "   |   |   "
   puts "-----------"
