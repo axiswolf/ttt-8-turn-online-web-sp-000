39460237c7938b293cdb07696ec880e31bfbfe46
Karen V <karenvo@usc.edu>
2019-01-31 14:56:54 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,24 @@
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+def input_to_index(input)
+  index = '#{input}' - 1
+end
+def valid_move?(move)
+
+end
+def turn(board)
+  puts "Please enter 1-9:" # ask for input
+  input = gets.chomp # get input
+  # convert input to index
+  index = input_to_index(input)
+  # if index is valid
+  # make the move for index
+  # show the board
+  # else
+  # ask for input agaain until you get valid input
+end
